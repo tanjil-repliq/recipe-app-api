d0a362e7fa50141049299f7b489cc0b13d75abbe
tanjil-repliq <md.tanjil@repliq.dev>
2025-02-13 11:40:41 +0600

    Fix linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 
 from app import calc
 
-class CalcTests(TestCase):
 
+class CalcTests(TestCase):
 
     # Test the calc module
     def test_add_numbers(self):
@@ -14,8 +14,8 @@ class CalcTests(TestCase):
 
         self.assertEqual(res, 11)
 
-
     def test_subtract_numbers(self):
         res = calc.subtract(5, 2)
 
         self.assertEqual(res, 3)
+    
