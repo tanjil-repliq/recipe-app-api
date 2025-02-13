db3d239c9bb44e0848fbcb7d14150f1c4a80d1e1
tanjil-repliq <md.tanjil@repliq.dev>
2025-02-13 11:37:52 +0600

    Fix linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,5 +3,6 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,13 +7,15 @@ from app import calc
 
 class CalcTests(TestCase):
 
+
     # Test the calc module
     def test_add_numbers(self):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
-    def test_subtract_numbers(self):
-        res= calc.subtract(5,2)
 
-        self.assertEqual(res,3)
+    def test_subtract_numbers(self):
+        res = calc.subtract(5, 2)
+
+        self.assertEqual(res, 3)
