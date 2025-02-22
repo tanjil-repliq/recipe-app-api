f2b72aaf7f8130c5bd0aaeaae17b1d02af4c954d
tanjil-repliq <md.tanjil@repliq.dev>
2025-02-22 16:18:14 +0600

    Implement user add edit functionalities from django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
@@ -8,14 +7,50 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     ordering = ["id"]
-    list_display = ["email", "name"]
+    list_display = ["email", "name", "last_login"]
 
-    fieldSets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")})),
-        (_("Important dates", {"fields": ("last_login")})),
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ("last_login",)
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
